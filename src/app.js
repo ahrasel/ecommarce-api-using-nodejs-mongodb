60fa8f4c14ce6fc6e13bf3b570f2081886f129f1
ahrasel <ahrasel95@gmail.com>
2022-01-29 17:33:08 +0600

    mongodb connection tested

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+//config dotenv
+dotenv.config();
+
+const app = express();
+
+// create mongo mongoose instance and connect to the local DB
+const mongoDB = process.env.MONGODB_URI;
+mongoose
+  .connect(mongoDB, { useNewUrlParser: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB: ", err);
+  });
+
+// express routes
+const usersRoutes = require("./routes/users-routes");
+// const authRoutes = require("./routes/auth-routes");
+
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+app.use("/api/users", usersRoutes);
+
+// listen server on port 5000
+app.listen(process.env.APP_PORT || 5000, () => {
+  console.log(`listening on port ${process.env.APP_PORT}`);
+});
