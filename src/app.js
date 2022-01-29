d0b36f335824fa985127ea9cde03f1c3a99e1f27
ahrasel <ahrasel95@gmail.com>
2022-01-29 21:20:22 +0600

    products routes added

--- src/app.js
+++ src/app.js
@@ -24,6 +24,7 @@ app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", require("./routes/auth-routes"));
 app.use("/api/users", require("./routes/user-routes"));
+app.use("/api/products", require("./routes/product-routes"));
 
 // listen server on port 5000
 app.listen(process.env.APP_PORT || 5000, () => {
--- src/routes/product-routes.js
+++ src/routes/product-routes.js
@@ -1,5 +1,61 @@
 // require express router
 const router = require("express").Router();
+const Product = require("../models/product");
+const { auth } = require("../middleware/auth");
+
+// create new product
+router.post("/", auth, async (req, res) => {
+  try {
+    const product = new Product(req.body);
+    const newProduct = await product.save();
+    return res.status(201).json(newProduct);
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+});
+
+// update product by id
+router.put("/:id", auth, async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    return res.status(200).json(product);
+  } catch (error) {
+    return res.status(500).json({ error: error, message: "Error updating product" });
+  }
+});
+
+// delete product by id
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+    return res.status(200).json(product);
+  } catch (error) {
+    return res.status(500).json({ error: error, message: "Error deleting product" });
+  }
+});
+
+// get all products
+router.get("/", auth, async (req, res) => {
+  try {
+    const products = await Product.find();
+    return res.status(200).json(products);
+  } catch (error) {
+    return res.status(500).json({ error: error, message: "Error getting products" });
+  }
+});
+
+// get product by id
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    return res.status(200).json(product);
+  } catch (error) {
+    return res.status(500).json({ error: error, message: "Error getting product" });
+  }
+});
 
 // export the router
 module.exports = router;
